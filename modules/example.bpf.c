6c5d53cdf3b2d64fb20c6cadd54de88126a21890
p3nj <w.benji@tuta.io>
2023-10-06 00:27:16 +1000

    complete kerlockhomes loading mech

--- modules/example.bpf.c
+++ modules/example.bpf.c
@@ -1,17 +1,9 @@
 #include <linux/bpf.h>
-#include <linux/if_ether.h>
-#include <linux/ip.h>
+#include <bpf/bpf_helpers.h>
 
-SEC("filter")
-int example_prog(struct __sk_buff *skb) {
-  struct ethhdr *eth = bpf_hdr_pointer_cast(skb->data, 0);
-  struct iphdr *ip = bpf_hdr_pointer_cast(eth + 1, 0);
-
-  if (ip->protocol == IPPROTO_ICMP) {
-    return XDP_DROP;
-  }
-
-  return XDP_PASS;
+SEC("xdp")
+int hello_world(struct __sk_buff *skb) {
+	return 0;
 }
 
 char _license[] SEC("license") = "GPL";
---
+++ src/kerlockhomes.c
@@ -0,0 +1,105 @@
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define MAX_BPF_OBJECTS 128
+
+struct bpf_object *loaded_objects[MAX_BPF_OBJECTS];
+int loaded_count = 0;
+
+void unload_bpf_programs() {
+    printf("Unloading BPF programs...\n");
+    for (int i = 0; i < loaded_count; ++i) {
+        bpf_object__close(loaded_objects[i]);
+    }
+    exit(0);
+}
+
+void signal_handler(int signum) {
+    unload_bpf_programs();
+}
+
+int load_bpf_program(const char *bpf_file) {
+    struct bpf_object *obj;
+    int err;
+
+    obj = bpf_object__open_file(bpf_file, NULL);
+    if (!obj) {
+        fprintf(stderr, "Failed to open BPF object file %s\n", bpf_file);
+        return 1;
+    }
+
+    err = bpf_object__load(obj);
+    if (err) {
+        fprintf(stderr, "Failed to load BPF object file %s: %s\n", bpf_file, strerror(-err));
+        bpf_object__close(obj);
+        return 1;
+    }
+
+    if (loaded_count < MAX_BPF_OBJECTS) {
+        loaded_objects[loaded_count++] = obj;
+    } else {
+        fprintf(stderr, "Reached maximum number of loaded BPF objects.\n");
+        bpf_object__close(obj);
+    }
+
+    printf("Successfully loaded %s\n", bpf_file);
+    return 0;
+}
+
+void read_trace_pipe() {
+	FILE *fp = fopen("/sys/kernel/debug/tracing/trace_pipe", "r");
+    if (fp == NULL) {
+        perror("Failed to open trace_pipe");
+		return;
+	}
+
+	char buffer[1024];
+	while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+		printf("%s", buffer);
+    }
+
+	fclose(fp);
+}
+
+
+int main() {
+    struct dirent *entry;
+    DIR *dp = opendir(".");
+    struct stat st;
+
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+
+    if (dp == NULL) {
+        perror("Failed to open directory");
+        return 1;
+    }
+
+    while ((entry = readdir(dp))) {
+        if (stat(entry->d_name, &st) == 0 && S_ISREG(st.st_mode) &&
+            strstr(entry->d_name, ".bpf.o") != NULL) {
+            printf("Loading %s...\n", entry->d_name);
+            if (load_bpf_program(entry->d_name) != 0) {
+                fprintf(stderr, "Failed to load %s, continuing...\n", entry->d_name);
+            }
+        }
+    }
+
+    closedir(dp);
+
+    printf("Press Ctrl+C to unload BPF programs and exit.\n");
+    while (1) {
+        read_trace_pipe();
+        sleep(1);
+    }
+
+    return 0;
+}
